2e5b8349dd5ac6e3a64caf69b9b8d4970c0f2cf2
ashymoniak <andryshimonyak@gmail.com>
2022-04-01 17:08:04 +0300

    PK, FK data types changed, Not null fields added

--- src/main/resources/sql/script.sql
+++ src/main/resources/sql/script.sql
@@ -8,7 +8,7 @@ CREATE SCHEMA public;
 CREATE SEQUENCE user_sequence INCREMENT BY 1 MINVALUE 0 MAXVALUE 9223372036854775807 START 100 NO CYCLE;
 CREATE TABLE application_user
 (
-    user_id         NUMERIC(16)  NOT NULL DEFAULT NEXTVAL('user_sequence'::regclass),
+    user_id         BIGINT       NOT NULL DEFAULT NEXTVAL('user_sequence'::regclass),
     username        VARCHAR(100) NOT NULL UNIQUE,
     password        VARCHAR(100) NOT NULL,
     role            VARCHAR(20)  NOT NULL,
@@ -20,22 +20,23 @@ CREATE TABLE application_user
 CREATE SEQUENCE person_sequence INCREMENT BY 1 MINVALUE 0 MAXVALUE 9223372036854775807 START 100 NO CYCLE;
 CREATE TABLE person
 (
-    person_id NUMERIC(16) NOT NULL DEFAULT NEXTVAL('person_sequence'::regclass),
+    person_id BIGINT NOT NULL DEFAULT NEXTVAL('person_sequence'::regclass),
     name      VARCHAR(100),
     surname   VARCHAR(100),
     email     VARCHAR(100),
-    user_id   NUMERIC(16) NOT NULL,
+    user_id   BIGINT NOT NULL,
     CONSTRAINT PK_Person PRIMARY KEY (person_id),
     CONSTRAINT FK_Person_ApplicationUser FOREIGN KEY (user_id) REFERENCES application_user (user_id),
-    CONSTRAINT UQ_Person_UserId UNIQUE (user_id)
+    CONSTRAINT UQ_Person_UserId UNIQUE (user_id),
+    CONSTRAINT UQ_Person_Email UNIQUE (email)
 );
 
 -- Objects to be recognized (tank, atv, ...)
 CREATE SEQUENCE observed_object_sequence INCREMENT BY 1 MINVALUE 0 MAXVALUE 9223372036854775807 START 100 NO CYCLE;
 CREATE TABLE observed_object
 (
-    observed_object_id NUMERIC(16) NOT NULL DEFAULT NEXTVAL('observed_object_sequence'::regclass),
-    object_name        VARCHAR(50),
+    observed_object_id BIGINT      NOT NULL DEFAULT NEXTVAL('observed_object_sequence'::regclass),
+    object_name        VARCHAR(50) NOT NULL,
     CONSTRAINT PK_ObservedObject PRIMARY KEY (observed_object_id)
 );
 
@@ -43,10 +44,10 @@ CREATE TABLE observed_object
 CREATE SEQUENCE media_sequence INCREMENT BY 1 MINVALUE 0 MAXVALUE 9223372036854775807 START 100 NO CYCLE;
 CREATE TABLE media
 (
-    media_id            NUMERIC(16) NOT NULL DEFAULT NEXTVAL('media_sequence'::regclass),
-    original_media_url  VARCHAR(500),
-    processed_media_url VARCHAR(500),
-    create_user         NUMERIC(16),
+    media_id            BIGINT       NOT NULL DEFAULT NEXTVAL('media_sequence'::regclass),
+    original_media_url  VARCHAR(500) NOT NULL,
+    processed_media_url VARCHAR(500) NOT NULL,
+    create_user         BIGINT,
     create_date         TIMESTAMP,
     CONSTRAINT PK_Media PRIMARY KEY (media_id),
     CONSTRAINT FK_Media_ApplicationUser FOREIGN KEY (create_user) REFERENCES application_user (user_id)
@@ -56,9 +57,9 @@ CREATE TABLE media
 CREATE SEQUENCE location_sequence INCREMENT BY 1 MINVALUE 0 MAXVALUE 9223372036854775807 START 100 NO CYCLE;
 CREATE TABLE location
 (
-    location_id NUMERIC(16) NOT NULL DEFAULT NEXTVAL('location_sequence'::regclass),
-    longitude   VARCHAR(100),
-    latitude    VARCHAR(100),
+    location_id BIGINT       NOT NULL DEFAULT NEXTVAL('location_sequence'::regclass),
+    longitude   VARCHAR(100) NOT NULL,
+    latitude    VARCHAR(100) NOT NULL,
     CONSTRAINT PK_Location PRIMARY KEY (location_id)
 );
 
@@ -66,12 +67,12 @@ CREATE TABLE location
 CREATE SEQUENCE ml_model_sequence INCREMENT BY 1 MINVALUE 0 MAXVALUE 9223372036854775807 START 100 NO CYCLE;
 CREATE TABLE ml_model
 (
-    ml_model_id NUMERIC(16) NOT NULL DEFAULT NEXTVAL('ml_model_sequence'::regclass),
-    model_name  VARCHAR(100),
-    model_path  VARCHAR(200),
+    ml_model_id BIGINT       NOT NULL DEFAULT NEXTVAL('ml_model_sequence'::regclass),
+    model_name  VARCHAR(100) NOT NULL,
+    model_path  VARCHAR(200) NOT NULL,
     is_custom   NUMERIC(1),
     is_active   NUMERIC(1),
-    create_user NUMERIC(16),
+    create_user BIGINT,
     create_date TIMESTAMP,
     CONSTRAINT PK_MlModel PRIMARY KEY (ml_model_id),
     CONSTRAINT FK_MlModel_ApplicationUser FOREIGN KEY (create_user) REFERENCES application_user (user_id)
@@ -81,9 +82,9 @@ CREATE TABLE ml_model
 CREATE SEQUENCE ml_model_observed_object_sequence INCREMENT BY 1 MINVALUE 0 MAXVALUE 9223372036854775807 START 100 NO CYCLE;
 CREATE TABLE ml_model_observed_object
 (
-    ml_model_observed_object_id NUMERIC(16) NOT NULL DEFAULT NEXTVAL('ml_model_observed_object_sequence'::regclass),
-    ml_model_id                 NUMERIC(16),
-    observed_object_id          NUMERIC(16),
+    ml_model_observed_object_id BIGINT NOT NULL DEFAULT NEXTVAL('ml_model_observed_object_sequence'::regclass),
+    ml_model_id                 BIGINT,
+    observed_object_id          BIGINT,
     CONSTRAINT PK_MlModelObservedObject PRIMARY KEY (ml_model_observed_object_id),
     CONSTRAINT FK_MlModelObservedObject_MlModel FOREIGN KEY (ml_model_id) REFERENCES ml_model (ml_model_id),
     CONSTRAINT FK_MlModelObservedObject_ObservedObject FOREIGN KEY (observed_object_id) REFERENCES observed_object (observed_object_id)
@@ -93,16 +94,16 @@ CREATE TABLE ml_model_observed_object
 CREATE SEQUENCE recognition_result_sequence INCREMENT BY 1 MINVALUE 0 MAXVALUE 9223372036854775807 START 100 NO CYCLE;
 CREATE TABLE recognition_result
 (
-    recognition_result_id  NUMERIC(16) NOT NULL DEFAULT NEXTVAL('recognition_result_sequence'::regclass),
+    recognition_result_id  BIGINT NOT NULL DEFAULT NEXTVAL('recognition_result_sequence'::regclass),
     description            VARCHAR(500),
     is_object_present      NUMERIC(1),
     is_recognition_correct NUMERIC(1),
-    ml_model_id            NUMERIC(16),
-    media_id               NUMERIC(16),
-    location_id            NUMERIC(16),
-    create_user            NUMERIC(16),
+    ml_model_id            BIGINT,
+    media_id               BIGINT,
+    location_id            BIGINT,
+    create_user            BIGINT,
     create_date            TIMESTAMP,
-    update_user            NUMERIC(16),
+    update_user            BIGINT,
     update_date            TIMESTAMP,
     CONSTRAINT PK_RecognitionResult PRIMARY KEY (recognition_result_id),
     CONSTRAINT FK_RecognitionResult_MlModel FOREIGN KEY (ml_model_id) REFERENCES ml_model (ml_model_id),
@@ -116,9 +117,9 @@ CREATE TABLE recognition_result
 CREATE SEQUENCE recognition_result_observed_object_sequence INCREMENT BY 1 MINVALUE 0 MAXVALUE 9223372036854775807 START 100 NO CYCLE;
 CREATE TABLE recognition_result_observed_object
 (
-    recognition_result_observed_object_id NUMERIC(16) NOT NULL DEFAULT NEXTVAL('recognition_result_observed_object_sequence'::regclass),
-    recognition_result_id                 NUMERIC(16),
-    observed_object_id                    NUMERIC(16),
+    recognition_result_observed_object_id BIGINT NOT NULL DEFAULT NEXTVAL('recognition_result_observed_object_sequence'::regclass),
+    recognition_result_id                 BIGINT,
+    observed_object_id                    BIGINT,
     CONSTRAINT PK_RecognitionResultObservedObject PRIMARY KEY (recognition_result_observed_object_id),
     CONSTRAINT FK_RecognitionResultObservedObject_RecognitionResult FOREIGN KEY (recognition_result_id) REFERENCES recognition_result (recognition_result_id),
     CONSTRAINT FK_RecognitionResultObservedObject_ObservedObject FOREIGN KEY (observed_object_id) REFERENCES observed_object (observed_object_id)
